1ab1f7c8c4a6b05bbfeb20242343e01df0bca25d
jarirliu1986 <289374247@qq.com>
2016-12-07 23:14:53 -0800

    refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -33,31 +34,31 @@ app.get('/todos', function (req, res) {
 //GET the single todo
 app.get('/todos/:id', function (req, res) {
 	//res.send('asking for id ' + req.params.id);
-	var todoId = req.params.id;
-	var noMatch = true;
-	var matchedTodo;
+	var todoId = parseInt(req.params.id);
+	console.log(todos);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
 
-	todos.forEach(function (todo) {
-		if(todo.id == todoId){
-			noMatch = false;
-			matchedTodo = todo;
-		}
-	})
-	if(noMatch){
-		res.status(404).send();
-	}else{
+	if(matchedTodo){
 		res.json(matchedTodo);
+	}else{
+		res.status(404).send();
 	}
 })
 
 //POST can take data /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
-	// console.log('description ' + body.description);
-	// console.log(body);
-	// res.json(body);
+	var body = _.pick(req.body, 'description', 'completed');
+
+	//input validation
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(400).send();
+	}
+
+	body.description = body.description.trim();
 	body.id = todoNextId++;
+
 	todos.push(body);
+	
 	res.json(body);
 })
 
